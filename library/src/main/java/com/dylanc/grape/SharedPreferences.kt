548ca259ed8238e568443db276628210ecb39326
Dylanc <caishenglang@foxmail.com>
2021-01-26 11:21:10 +0800

    Optimize SharedPreferences.kt

--- library/src/main/java/com/dylanc/grape/SharedPreferences.kt
+++ library/src/main/java/com/dylanc/grape/SharedPreferences.kt
@@ -11,21 +11,22 @@ import kotlin.reflect.KProperty
  * @author Dylan Cai
  */
 
-val defaultSharedPreferences: SharedPreferences by lazy { sharedPreferencesOf() }
+var defaultSharedPreferences = sharedPreferencesOf()
+val defaultSharedPreferencesOwner by lazy { object : SharedPreferencesOwner {} }
 
 fun sharedPreferencesOf(name: String = packageName, mode: Int = Context.MODE_PRIVATE): SharedPreferences =
   application.getSharedPreferences(name, mode)
 
-fun <T : Any> Any.sharedPreferences(key: String, default: T) =
-  SharedPreferencesValueDelegate(key, default, sharedPreferencesOwner.sharedPreferences)
+fun <T : Any> Any.sharedPreferences(key: String? = null, default: T) =
+  SharedPreferencesValueWithDefault(key, default, sharedPreferencesOwner.sharedPreferences)
 
-fun <T : Any> SharedPreferencesOwner.sharedPreferences(key: String, default: T) =
-  SharedPreferencesValueDelegate(key, default, sharedPreferences)
+fun Any.sharedPreferences(key: String? = null) =
+  SharedPreferencesStringValue(key, sharedPreferencesOwner.sharedPreferences)
 
 private val Any.sharedPreferencesOwner: SharedPreferencesOwner
-  get() = object : SharedPreferencesOwner {}
+  get() = if (this is SharedPreferencesOwner) this else defaultSharedPreferencesOwner
 
-fun <T> SharedPreferences.putValue(key: String, value: T) {
+fun <T : Any> SharedPreferences.put(key: String, value: T) {
   edit().apply {
     when (value) {
       is Long -> putLong(key, value)
@@ -33,13 +34,16 @@ fun <T> SharedPreferences.putValue(key: String, value: T) {
       is Int -> putInt(key, value)
       is Boolean -> putBoolean(key, value)
       is Float -> putFloat(key, value)
-      else -> throw IllegalArgumentException()
+      else -> {
+        val valueType = value.javaClass.canonicalName
+        throw IllegalArgumentException("Illegal value type $valueType for key \"$key\"")
+      }
     }
   }.apply()
 }
 
 @Suppress("UNCHECKED_CAST")
-fun <T : Any> SharedPreferences.getValue(key: String, default: T) =
+fun <T : Any> SharedPreferences.getOrDefault(key: String, default: T) =
   run {
     when (default) {
       is Long -> getLong(key, default)
@@ -58,17 +62,30 @@ interface SharedPreferencesOwner {
   val sharedPreferences: SharedPreferences get() = defaultSharedPreferences
 }
 
-class SharedPreferencesValueDelegate<T : Any>(
-  private val key: String,
+class SharedPreferencesStringValue(
+  private val key: String? = null,
+  private val sharedPreferences: SharedPreferences
+) : ReadWriteProperty<SharedPreferencesOwner, String?> {
+
+  override fun setValue(thisRef: SharedPreferencesOwner, property: KProperty<*>, value: String?) {
+    sharedPreferences.edit().putString(key ?: property.name, value).apply()
+  }
+
+  override fun getValue(thisRef: SharedPreferencesOwner, property: KProperty<*>): String? {
+    return sharedPreferences.getString(key ?: property.name, null)
+  }
+}
+
+class SharedPreferencesValueWithDefault<T : Any>(
+  private val key: String? = null,
   private val default: T,
-  private val sharedPreferences: SharedPreferences = defaultSharedPreferences
+  private val sharedPreferences: SharedPreferences
 ) : ReadWriteProperty<Any, T> {
 
   override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
-    sharedPreferences.putValue(key, value)
+    sharedPreferences.put(key ?: property.name, value)
   }
-
   override fun getValue(thisRef: Any, property: KProperty<*>): T {
-    return sharedPreferences.getValue(key, default)
+    return sharedPreferences.getOrDefault(key ?: property.name, default)
   }
 }
