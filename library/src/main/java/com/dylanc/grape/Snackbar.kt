fbf8ae2f0a16924eacfb881d919d8158a4527f29
Dylanc <caishenglang@foxmail.com>
2021-02-04 15:04:37 +0800

    Support to set custom view for snackbar

--- library/src/main/java/com/dylanc/grape/Snackbar.kt
+++ library/src/main/java/com/dylanc/grape/Snackbar.kt
@@ -3,10 +3,14 @@
 package com.dylanc.grape
 
 import android.app.Activity
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.LayoutRes
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
 
 /**
  * @author Dylan Cai
@@ -47,3 +51,31 @@ fun View.longSnackbar(text: String, block: Snackbar.() -> Unit = {}) =
 
 fun View.longSnackbar(@StringRes resId: Int, block: Snackbar.() -> Unit = {}) =
   Snackbar.make(this, resId, Snackbar.LENGTH_LONG).apply(block).apply { show() }
+
+fun Snackbar.setCustomView(@LayoutRes layoutId: Int, onBindView: View.() -> Unit = {}) {
+  val parent = view as Snackbar.SnackbarLayout
+  val contentView = parent.getChildAt(0) as SnackbarContentLayout
+  val inflater = LayoutInflater.from(view.context)
+  val decorView = inflater.inflate(R.layout.design_layout_snackbar_include, contentView, false) as SnackbarContentLayout
+  decorView.apply {
+    addView(inflater.inflate(layoutId, contentView, false).apply(onBindView))
+    id = contentView.id
+    findViewById<TextView>(R.id.snackbar_text).apply {
+      id = com.google.android.material.R.id.snackbar_text
+      text = contentView.findViewById<TextView>(id).text.toString()
+    }
+    findViewById<View>(R.id.snackbar_action).apply {
+      id = com.google.android.material.R.id.snackbar_action
+    }
+  }
+  try {
+    val method = decorView::class.java.getDeclaredMethod("onFinishInflate")
+    method.isAccessible = true
+    method.invoke(decorView)
+  } catch (e: Exception) {
+  }
+  parent.apply {
+    removeView(contentView)
+    addView(decorView, indexOfChild(contentView))
+  }
+}
