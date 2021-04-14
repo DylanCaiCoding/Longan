aca617da4ea2645d37c9d56bef82abb911528739
Dylanc <caishenglang@foxmail.com>
2021-04-14 17:57:04 +0800

    Add more intent to share

--- library/src/main/java/com/dylanc/grape/Intent.kt
+++ library/src/main/java/com/dylanc/grape/Intent.kt
@@ -5,23 +5,21 @@ package com.dylanc.grape
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.IntentSender
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import androidx.core.content.FileProvider
 import androidx.core.os.bundleOf
 import java.io.File
+
 
 /**
  * @author Dylan Cai
  */
 
+inline fun <reified T> Context.intentOf(vararg pairs: Pair<String, *>) =
+  intentOf<T>(bundleOf(*pairs))
+
 inline fun <reified T> Context.intentOf(bundle: Bundle) =
   Intent(this, T::class.java).apply { putExtras(bundle) }
-
-inline fun <reified T> Context.intentOf(vararg pairs: Pair<String, *>) =
-  Intent(this, T::class.java).apply { putExtras(bundleOf(*pairs)) }
 
 inline fun Intent.clearTask(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) }
 
@@ -41,6 +39,9 @@ inline fun Intent.noHistory(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_NO
 
 inline fun Intent.singleTop(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP) }
 
+inline fun Intent.createChooser(title: String? = null): Intent =
+  Intent.createChooser(this, title)
+
 inline fun <reified T> Activity.intentExtras(name: String): Lazy<T?> = lazy {
   intent.extras[name]
 }
@@ -53,23 +54,40 @@ inline fun <reified T> Activity.safeIntentExtras(name: String): Lazy<T> = lazy {
   checkNotNull(intent.extras[name]) { "No intent value for key \"$name\"" }
 }
 
-inline fun sharePdfIntentOf(pdfFile: File, title: String? = null, sender: IntentSender? = null): Intent {
-  val pdfUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-    FileProvider.getUriForFile(application, "$packageName.provider", pdfFile)
-  } else {
-    Uri.fromFile(pdfFile)
-  }
-  return sharePdfIntentOf(pdfUri, title, sender)
-}
+inline val sendIntent get() = Intent(Intent.ACTION_SEND)
 
-inline fun sharePdfIntentOf(pdfUri: Uri, title: String? = null, sender: IntentSender? = null): Intent =
-  Intent(Intent.ACTION_SEND).apply {
+inline fun shareTextIntentOf(content: String, title: String? = null) =
+  sendIntent.run {
+    type = "text/plain"
+    putExtra(Intent.EXTRA_TEXT, content)
+    createChooser(title)
+  }
+
+inline fun shareImageIntentOf(imageUri: Uri, title: String? = null) =
+  shareTextImageIntentOf(null, imageUri, title)
+
+inline fun shareTextImageIntentOf(content: String?, imageUri: Uri, title: String? = null) =
+  sendIntent.run {
+    type = "image/*"
+    putExtra(Intent.EXTRA_TEXT, content)
+    putExtra(Intent.EXTRA_STREAM, imageUri)
+    createChooser(title)
+  }
+
+inline fun shareTextImageIntentOf(content: String?, uris: ArrayList<Uri>, title: String? = null) =
+  sendIntent.run {
+    type = "image/*"
+    putExtra(Intent.EXTRA_TEXT, content)
+    putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris)
+    createChooser(title)
+  }
+
+inline fun sharePdfIntentOf(pdfFile: File, title: String? = null) =
+  sharePdfIntentOf(pdfFile.toUri(), title)
+
+inline fun sharePdfIntentOf(pdfUri: Uri, title: String? = null) =
+  sendIntent.run {
     type = "application/pdf"
     putExtra(Intent.EXTRA_STREAM, pdfUri)
-  }.let {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-      Intent.createChooser(it, title, sender)
-    } else {
-      it
-    }
+    createChooser(title)
   }
