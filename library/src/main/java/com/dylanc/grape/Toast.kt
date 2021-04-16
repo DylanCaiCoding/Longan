0246a4216711249efb4df465839ee3e2c3dbe461
Dylanc <caishenglang@foxmail.com>
2021-04-16 15:23:15 +0800

    Fix toast's BadTokenException

--- library/src/main/java/com/dylanc/grape/Toast.kt
+++ library/src/main/java/com/dylanc/grape/Toast.kt
@@ -4,19 +4,20 @@ package com.dylanc.grape
 
 import android.widget.Toast
 import androidx.annotation.StringRes
+import me.drakeet.support.toast.ToastCompat
 
 /**
  * @author Dylan Cai
  */
 
 inline fun toast(text: String?, duration: Int = Toast.LENGTH_SHORT, block: Toast.() -> Unit = {}): Toast =
-  Toast.makeText(application, text, duration).apply(block).apply { show() }
+  ToastCompat.makeText(application, text, duration).apply(block).apply { show() }
 
 inline fun toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT, block: Toast.() -> Unit = {}): Toast =
-  Toast.makeText(application, resId, duration).apply(block).apply { show() }
+  ToastCompat.makeText(application, resId, duration).apply(block).apply { show() }
 
 inline fun longToast(text: String?, block: Toast.() -> Unit = {}): Toast =
-  Toast.makeText(application, text, Toast.LENGTH_LONG).apply(block).apply { show() }
+  ToastCompat.makeText(application, text, Toast.LENGTH_LONG).apply(block).apply { show() }
 
 inline fun longToast(@StringRes resId: Int, block: Toast.() -> Unit = {}): Toast =
-  Toast.makeText(application, resId, Toast.LENGTH_LONG).apply(block).apply { show() }
+  ToastCompat.makeText(application, resId, Toast.LENGTH_LONG).apply(block).apply { show() }
