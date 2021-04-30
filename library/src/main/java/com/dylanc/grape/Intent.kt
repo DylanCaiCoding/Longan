3e7e51fb58b808aa3e8448bc9bbafd5866eaccfa
Dylanc <caishenglang@foxmail.com>
2021-04-30 20:15:41 +0800

    Add Share.kt

--- library/src/main/java/com/dylanc/grape/Intent.kt
+++ library/src/main/java/com/dylanc/grape/Intent.kt
@@ -2,13 +2,14 @@
 
 package com.dylanc.grape
 
+import android.Manifest.permission.CALL_PHONE
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.annotation.RequiresPermission
 import androidx.core.os.bundleOf
-import java.io.File
 
 
 /**
@@ -17,6 +18,9 @@ import java.io.File
 
 inline fun <reified T> Context.intentOf(vararg pairs: Pair<String, *>) =
   intentOf<T>(bundleOf(*pairs))
+
+inline fun intentOf(vararg pairs: Pair<String, *>) =
+  Intent().apply { putExtras(bundleOf(*pairs)) }
 
 inline fun <reified T> Context.intentOf(bundle: Bundle) =
   Intent(this, T::class.java).apply { putExtras(bundle) }
@@ -39,55 +43,29 @@ inline fun Intent.noHistory(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_NO
 
 inline fun Intent.singleTop(): Intent = apply { addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP) }
 
-inline fun Intent.createChooser(title: String? = null): Intent =
-  Intent.createChooser(this, title)
+inline fun Intent.createChooser(title: String? = null): Intent = Intent.createChooser(this, title)
 
-inline fun <reified T> Activity.intentExtras(name: String): Lazy<T?> = lazy {
+inline fun <reified T> Activity.intentExtras(name: String) = lazy<T?> {
   intent.extras[name]
 }
 
-inline fun <reified T> Activity.intentExtras(name: String, defaultValue: T): Lazy<T> = lazy {
+inline fun <reified T> Activity.intentExtras(name: String, defaultValue: T) = lazy {
   intent.extras[name] ?: defaultValue
 }
 
-inline fun <reified T> Activity.safeIntentExtras(name: String): Lazy<T> = lazy {
+inline fun <reified T> Activity.safeIntentExtras(name: String) = lazy<T> {
   checkNotNull(intent.extras[name]) { "No intent value for key \"$name\"" }
 }
 
-inline val sendIntent get() = Intent(Intent.ACTION_SEND)
+fun dialIntentOf(phoneNumber: String): Intent =
+  Intent(Intent.ACTION_DIAL, Uri.parse("tel:${Uri.encode(phoneNumber)}"))
 
-inline fun shareTextIntentOf(content: String, title: String? = null) =
-  sendIntent.run {
-    type = "text/plain"
-    putExtra(Intent.EXTRA_TEXT, content)
-    createChooser(title)
-  }
+@RequiresPermission(CALL_PHONE)
+inline fun callIntentOf(phoneNumber: String) =
+  Intent(Intent.ACTION_CALL, Uri.parse("tel:${Uri.encode(phoneNumber)}"))
 
-inline fun shareImageIntentOf(imageUri: Uri, title: String? = null) =
-  shareTextImageIntentOf(null, imageUri, title)
+inline fun sendSmsIntentOf(phoneNumber: String, content: String?) =
+  Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:${Uri.encode(phoneNumber)}"))
+    .apply { putExtra("sms_body", content) }
 
-inline fun shareTextImageIntentOf(content: String?, imageUri: Uri, title: String? = null) =
-  sendIntent.run {
-    type = "image/*"
-    putExtra(Intent.EXTRA_TEXT, content)
-    putExtra(Intent.EXTRA_STREAM, imageUri)
-    createChooser(title)
-  }
-
-inline fun shareTextImageIntentOf(content: String?, uris: ArrayList<Uri>, title: String? = null) =
-  sendIntent.run {
-    type = "image/*"
-    putExtra(Intent.EXTRA_TEXT, content)
-    putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris)
-    createChooser(title)
-  }
-
-inline fun sharePdfIntentOf(pdfFile: File, title: String? = null) =
-  sharePdfIntentOf(pdfFile.toUri(), title)
-
-inline fun sharePdfIntentOf(pdfUri: Uri, title: String? = null) =
-  sendIntent.run {
-    type = "application/pdf"
-    putExtra(Intent.EXTRA_STREAM, pdfUri)
-    createChooser(title)
-  }
+inline val launchIntent: Intent? get() = application.packageManager.getLaunchIntentForPackage(packageName)
