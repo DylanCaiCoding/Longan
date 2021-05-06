06353daa3263d0965702fc92b81ac4f1826797db
Dylanc <caishenglang@foxmail.com>
2021-05-06 18:52:03 +0800

    Optimize WindowInset, add Window.kt and NavigationBar.kt

--- library/src/main/java/com/dylanc/grape/Keyboard.kt
+++ library/src/main/java/com/dylanc/grape/Keyboard.kt
@@ -2,35 +2,19 @@
 
 package com.dylanc.grape
 
-import android.app.Activity
-import android.view.View
+import android.widget.EditText
 import androidx.core.view.WindowInsetsCompat.Type
-import androidx.fragment.app.Fragment
 
 /**
  * @author Dylan Cai
  */
 
-inline fun Activity.showKeyboard() = window.decorView.showKeyboard()
+inline fun EditText.showKeyboard() = windowInsetsControllerCompat?.show(Type.ime())
 
-inline fun Fragment.showKeyboard() = requireView().showKeyboard()
+inline fun EditText.hideKeyboard() = windowInsetsControllerCompat?.hide(Type.ime())
 
-inline fun View.showKeyboard() = windowInsetsControllerCompat?.show(Type.ime())
+inline fun EditText.toggleKeyboard() = if (isKeyboardVisible) hideKeyboard() else showKeyboard()
 
-inline fun Activity.hideKeyboard() = window.decorView.hideKeyboard()
+inline val EditText.isKeyboardVisible: Boolean get() = rootWindowInsetsCompat?.isVisible(Type.ime()) == true
 
-inline fun Fragment.hideKeyboard() = requireView().hideKeyboard()
-
-inline fun View.hideKeyboard() = windowInsetsControllerCompat?.hide(Type.ime())
-
-inline fun Activity.toggleKeyboard() = window.decorView.toggleKeyboard()
-
-inline fun Fragment.toggleKeyboard() = requireView().toggleKeyboard()
-
-inline fun View.toggleKeyboard() = if (isKeyboardVisible) hideKeyboard() else showKeyboard()
-
-inline val Activity.isKeyboardVisible: Boolean get() = window.decorView.isKeyboardVisible
-
-inline val Fragment.isKeyboardVisible: Boolean get() = requireView().isKeyboardVisible
-
-inline val View.isKeyboardVisible: Boolean get() = windowInsetsCompat?.isVisible(Type.ime()) == true
+inline val EditText.keyboardHeight get() = rootWindowInsetsCompat?.getInsets(Type.ime())?.bottom ?: -1
