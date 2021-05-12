db5af48e12c1ae2c5302dbf615935a0ead446279
Dylanc <caishenglang@foxmail.com>
2021-05-12 20:56:13 +0800

    Add PrintWriter.kt

---
+++ library/src/main/java/com/dylanc/grape/PrintWriter.kt
@@ -0,0 +1,14 @@
+package com.dylanc.grape
+
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
+
+/**
+ * @author Dylan Cai
+ */
+
+
+inline fun printTo(file: File, block: PrintWriter.() -> Unit) =
+  PrintWriter(BufferedWriter(FileWriter(file))).apply(block).close()
