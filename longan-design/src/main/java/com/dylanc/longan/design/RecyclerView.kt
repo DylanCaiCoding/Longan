301fd5d847c87d491b1ed55a62a5677da2574495
Dylanc <caishenglang@foxmail.com>
2021-09-29 17:47:54 +0800

    Supports RecyclerView to set an empty view

--- longan-design/src/main/java/com/dylanc/longan/design/RecyclerView.kt
+++ longan-design/src/main/java/com/dylanc/longan/design/RecyclerView.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021. Dylan Cai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress("unused", "NOTHING_TO_INLINE")
 
 package com.dylanc.longan.design
@@ -18,6 +34,34 @@ import androidx.recyclerview.widget.RecyclerView
  * @author Dylan Cai
  */
 
+inline fun RecyclerView.setEmptyView(owner: LifecycleOwner, emptyView: View) =
+  observeDataEmpty(owner) { emptyView.isVisible = it }
+
+inline fun RecyclerView.observeDataEmpty(owner: LifecycleOwner, noinline block: (Boolean) -> Unit) {
+  owner.lifecycle.addObserver(object : LifecycleObserver {
+    private var observer: RecyclerView.AdapterDataObserver? = null
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+      if (observer == null) {
+        val adapter = checkNotNull(adapter) {
+          "RecyclerView needs to set up the adapter before setting up an empty view."
+        }
+        observer = AdapterDataEmptyObserver(adapter, block)
+        adapter.registerAdapterDataObserver(observer!!)
+      }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+      observer?.let {
+        adapter?.unregisterAdapterDataObserver(it)
+        observer = null
+      }
+    }
+  })
+}
+
 inline fun RecyclerView.smoothScrollToStartPosition(position: Int) =
   smoothScrollToPosition(position, SNAP_TO_START)
 
@@ -37,42 +81,25 @@ inline fun LinearSmoothScroller(context: Context, snapPreference: Int) =
     override fun getHorizontalSnapPreference() = snapPreference
   }
 
-inline fun RecyclerView.Adapter<*>.observeDataEmpty(owner: LifecycleOwner, emptyView: View) {
-  val observer = AdapterDataEmptyObserver(this, emptyView)
-  owner.lifecycle.addObserver(object : LifecycleObserver {
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    fun onCreate() {
-      registerAdapterDataObserver(observer)
-    }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun onDestroy() {
-      unregisterAdapterDataObserver(observer)
-    }
-  })
-}
-
 class AdapterDataEmptyObserver(
   private val adapter: RecyclerView.Adapter<*>,
-  private val emptyView: View
+  private val checkEmpty: (Boolean) -> Unit
 ) : RecyclerView.AdapterDataObserver() {
 
   override fun onChanged() {
     super.onChanged()
-    checkEmpty()
+    checkEmpty(isDataEmpty)
   }
 
   override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
     super.onItemRangeInserted(positionStart, itemCount)
-    checkEmpty()
+    checkEmpty(isDataEmpty)
   }
 
   override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
     super.onItemRangeRemoved(positionStart, itemCount)
-    checkEmpty()
+    checkEmpty(isDataEmpty)
   }
 
-  private fun checkEmpty() {
-    emptyView.isVisible = adapter.itemCount == 0
-  }
+  private val isDataEmpty get() = adapter.itemCount == 0
 }
