47b27b0af26669e53b3b24e62eeba0568c96d111
DylanCaiCoding <caishenglang@foxmail.com>
2021-10-12 23:10:11 +0800

    Add ContentValues

--- longan/src/main/java/com/dylanc/longan/ContentResolver.kt
+++ longan/src/main/java/com/dylanc/longan/ContentResolver.kt
@@ -136,39 +136,3 @@ inline fun ContentResolver.delete(
   selectionArgs: Array<String>? = null
 ): Int =
   delete(uri, where, selectionArgs)
-
-inline fun contentValues(block: ContentValues.() -> Unit) = ContentValues().apply(block)
-
-inline var ContentValues.displayName: String?
-  get() = get(MediaStore.MediaColumns.DISPLAY_NAME) as String?
-  set(value) = put(MediaStore.MediaColumns.DISPLAY_NAME, value)
-
-inline var ContentValues.relativePath: String?
-  get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-    get(MediaStore.MediaColumns.RELATIVE_PATH) as String?
-  } else null
-  set(value) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-      put(MediaStore.MediaColumns.RELATIVE_PATH, value)
-    }
-  }
-
-inline var ContentValues.mimeType: String?
-  get() = get(MediaStore.MediaColumns.MIME_TYPE) as String?
-  set(value) = put(MediaStore.MediaColumns.MIME_TYPE, value)
-
-inline var ContentValues.imageDescription: String?
-  get() = get(MediaStore.Images.Media.DESCRIPTION) as String?
-  set(value) = put(MediaStore.Images.Media.DESCRIPTION, value)
-
-inline var ContentValues.videoDescription: String?
-  get() = get(MediaStore.Images.Media.DESCRIPTION) as String?
-  set(value) = put(MediaStore.Video.Media.DESCRIPTION, value)
-
-inline var ContentValues.dateAdded: Long?
-  get() = get(MediaStore.MediaColumns.DATE_ADDED) as Long?
-  set(value) = put(MediaStore.MediaColumns.DATE_ADDED, value)
-
-inline var ContentValues.dateModified: Long?
-  get() = get(MediaStore.MediaColumns.DATE_MODIFIED) as Long?
-  set(value) = put(MediaStore.MediaColumns.DATE_MODIFIED, value)
