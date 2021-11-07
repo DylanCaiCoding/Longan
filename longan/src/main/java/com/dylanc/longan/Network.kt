65a9fbbb7affaa01cacd54a4fbf168f200c79549
DylanCaiCoding <caishenglang@foxmail.com>
2021-11-07 22:55:09 +0800

    Optimize Network.kt

--- longan/src/main/java/com/dylanc/longan/Network.kt
+++ longan/src/main/java/com/dylanc/longan/Network.kt
@@ -19,49 +19,85 @@
 package com.dylanc.longan
 
 import android.Manifest.permission.ACCESS_NETWORK_STATE
+import android.Manifest.permission.ACCESS_WIFI_STATE
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkCapabilities.*
 import android.net.NetworkRequest
+import android.net.wifi.WifiManager
 import android.os.Build
 import androidx.annotation.RequiresPermission
 import androidx.lifecycle.LiveData
 
 
+inline val connectivityManager: ConnectivityManager?
+  get() = application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+
 @get:RequiresPermission(ACCESS_NETWORK_STATE)
-inline val isNetworkAvailable: Boolean
-  get() {
-    val connectivityManager = application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)?.run {
+val isNetworkAvailable: Boolean
+  get() = connectivityManager?.run {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      getNetworkCapabilities(activeNetwork)?.run {
         hasCapability(NET_CAPABILITY_INTERNET) && hasCapability(NET_CAPABILITY_VALIDATED)
       }
     } else {
       @Suppress("DEPRECATION")
-      connectivityManager.activeNetworkInfo?.isConnectedOrConnecting
-    } ?: false
-  }
+      activeNetworkInfo?.isConnectedOrConnecting
+    }
+  } ?: false
+
+@get:RequiresPermission(ACCESS_NETWORK_STATE)
+val isWifiConnected: Boolean
+  get() = connectivityManager?.run {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      getNetworkCapabilities(activeNetwork)?.hasTransport(TRANSPORT_WIFI)
+    } else {
+      @Suppress("DEPRECATION")
+      activeNetworkInfo?.run { isConnected && type == ConnectivityManager.TYPE_WIFI }
+    }
+  } ?: false
+
+@get:RequiresPermission(ACCESS_NETWORK_STATE)
+val isMobileData: Boolean
+  get() = connectivityManager?.run {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      getNetworkCapabilities(activeNetwork)?.hasTransport(TRANSPORT_CELLULAR)
+    } else {
+      @Suppress("DEPRECATION")
+      activeNetworkInfo?.run { isAvailable && type == ConnectivityManager.TYPE_MOBILE }
+    }
+  } ?: false
+
+@get:RequiresPermission(ACCESS_WIFI_STATE)
+inline val isWifiEnabled: Boolean
+  get() = (application.getSystemService(Context.WIFI_SERVICE) as WifiManager?)?.isWifiEnabled == true
 
 class NetworkAvailableLiveData @RequiresPermission(ACCESS_NETWORK_STATE) constructor() : LiveData<Boolean>() {
 
-  private val connectivityManager = application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+  private val connectivityManager = com.dylanc.longan.connectivityManager
 
   @RequiresPermission(ACCESS_NETWORK_STATE)
   override fun onActive() {
     super.onActive()
     when {
       Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ->
-        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+        connectivityManager?.registerDefaultNetworkCallback(networkCallback)
       else ->
-        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+        connectivityManager?.registerNetworkCallback(networkRequest, networkCallback)
     }
   }
 
   override fun onInactive() {
     super.onInactive()
-    connectivityManager.unregisterNetworkCallback(networkCallback)
+    connectivityManager?.unregisterNetworkCallback(networkCallback)
+  }
+
+  override fun setValue(value: Boolean) {
+    if (this.value != value) {
+      super.setValue(value)
+    }
   }
 
   private val networkRequest by lazy {
@@ -91,5 +127,4 @@ class NetworkAvailableLiveData @RequiresPermission(ACCESS_NETWORK_STATE) constru
       postValue(false)
     }
   }
-
 }
