b729f2340e77da708185fc667e75e0c9af3170c2
Dylanc <caishenglang@foxmail.com>
2021-12-16 14:46:32 +0800

    Add Application#doOnActivityLifecycle(...)

--- longan/src/main/java/com/dylanc/longan/AppInitializer.kt
+++ longan/src/main/java/com/dylanc/longan/AppInitializer.kt
@@ -18,10 +18,8 @@
 
 package com.dylanc.longan
 
-import android.app.Activity
 import android.app.Application
 import android.content.Context
-import android.os.Bundle
 import androidx.startup.Initializer
 
 
@@ -29,31 +27,17 @@ internal class AppInitializer : Initializer<Unit> {
 
   override fun create(context: Context) {
     application = context as Application
-    application.registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
+    application.doOnActivityLifecycle(
+      onActivityCreated = { activity, _ ->
+        activityCache.add(activity)
+      },
+      onActivityDestroyed = { activity ->
+        activityCache.remove(activity)
+      }
+    )
   }
 
   override fun dependencies() = emptyList<Class<Initializer<*>>>()
-
-  private val activityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
-
-    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-      activityCache.add(activity)
-    }
-
-    override fun onActivityStarted(activity: Activity) = Unit
-
-    override fun onActivityResumed(activity: Activity) = Unit
-
-    override fun onActivityPaused(activity: Activity) = Unit
-
-    override fun onActivityStopped(activity: Activity) = Unit
-
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
-
-    override fun onActivityDestroyed(activity: Activity) {
-      activityCache.remove(activity)
-    }
-  }
 
   companion object {
     internal lateinit var application: Application private set
--- longan/src/main/java/com/dylanc/longan/Lifecycle.kt
+++ longan/src/main/java/com/dylanc/longan/Lifecycle.kt
@@ -18,37 +18,86 @@
 
 package com.dylanc.longan
 
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 
 
-inline fun Fragment.doOnViewLifecycle(
-  crossinline onCreateView: () -> Unit = {},
-  crossinline onStart: () -> Unit = {},
-  crossinline onResume: () -> Unit = {},
-  crossinline onPause: () -> Unit = {},
-  crossinline onStop: () -> Unit = {},
-  crossinline onDestroyView: () -> Unit = {},
+fun Application.doOnActivityLifecycle(
+  onActivityCreated: ((Activity, Bundle?) -> Unit)? = null,
+  onActivityStarted: ((Activity) -> Unit)? = null,
+  onActivityResumed: ((Activity) -> Unit)? = null,
+  onActivityPaused: ((Activity) -> Unit)? = null,
+  onActivityStopped: ((Activity) -> Unit)? = null,
+  onActivitySaveInstanceState: ((Activity, Bundle?) -> Unit)? = null,
+  onActivityDestroyed: ((Activity) -> Unit)? = null,
+): Application.ActivityLifecycleCallbacks =
+  object : Application.ActivityLifecycleCallbacks {
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+      onActivityCreated?.invoke(activity, savedInstanceState)
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+      onActivityStarted?.invoke(activity)
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+      onActivityResumed?.invoke(activity)
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+      onActivityPaused?.invoke(activity)
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+      onActivityStopped?.invoke(activity)
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+      onActivitySaveInstanceState?.invoke(activity, outState)
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+      onActivityDestroyed?.invoke(activity)
+    }
+  }.also {
+    registerActivityLifecycleCallbacks(it)
+  }
+
+fun Fragment.doOnViewLifecycle(
+  onCreateView: (() -> Unit)? = null,
+  onStart: (() -> Unit)? = null,
+  onResume: (() -> Unit)? = null,
+  onPause: (() -> Unit)? = null,
+  onStop: (() -> Unit)? = null,
+  onDestroyView: (() -> Unit)? = null,
 ) =
   viewLifecycleOwner.doOnLifecycle(onCreateView, onStart, onResume, onPause, onStop, onDestroyView)
 
-inline fun LifecycleOwner.doOnLifecycle(
-  crossinline onCreate: () -> Unit = {},
-  crossinline onStart: () -> Unit = {},
-  crossinline onResume: () -> Unit = {},
-  crossinline onPause: () -> Unit = {},
-  crossinline onStop: () -> Unit = {},
-  crossinline onDestroy: () -> Unit = {},
+fun LifecycleOwner.doOnLifecycle(
+  onCreate: (() -> Unit)? = null,
+  onStart: (() -> Unit)? = null,
+  onResume: (() -> Unit)? = null,
+  onPause: (() -> Unit)? = null,
+  onStop: (() -> Unit)? = null,
+  onDestroy: (() -> Unit)? = null,
 ) =
   lifecycle.addObserver(object : DefaultLifecycleObserver {
-    fun onCreate() = onCreate()
-    fun onStart() = onStart()
-    fun onResume() = onResume()
-    fun onPause() = onPause()
-    fun onStop() = onStop()
-    fun onDestroy() = onDestroy()
+    fun onCreate() = onCreate?.invoke()
+
+    fun onStart() = onStart?.invoke()
+
+    fun onResume() = onResume?.invoke()
+
+    fun onPause() = onPause?.invoke()
+
+    fun onStop() = onStop?.invoke()
+
+    fun onDestroy() = onDestroy?.invoke()
   })
 
 val Fragment.viewLifecycleScope get() = viewLifecycleOwner.lifecycleScope
